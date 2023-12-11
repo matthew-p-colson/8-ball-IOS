59275fcff59319158bf96a70ebbecb24ac7ea71e
matthew colson <matthew.p.colson@gmail.com>
2023-12-10 21:38:04 -0600

    Updated layout

--- 8 Ball IOS/ViewController.swift
+++ 8 Ball IOS/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    let ballImages = [
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        ballImageView.image = ballImages[Int.random(in: 0..<5)]
+    }
 }
 
